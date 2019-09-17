8d964a3878f1d284ecc8c62c39e38b36729a6467
Xuejun Li <lixuejun1974@icloud.com>
2019-09-16 20:15:45 -0400

    Initial Commit

---
+++ FacialRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacialRecognition
+//
+//  Created by Xuejun Li on 9/16/19.
+//  Copyright © 2019 Xuejun Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
